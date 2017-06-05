9efb857f3c3c37bc43a25daf1857e7eb2bddd7b4
tamarakatic <tamarakatic@gmail.com>
2017-06-05 16:53:22 +0200

    Create Memeber

---
+++ Crew/Crew.CrewMemberActor.Interfaces/Member.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    [DataContract]
+    public class Member : IMember
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int Telephone { get; set; }
+
+        [DataMember]
+        public bool IsLead { get; set; }
+
+        [DataMember]
+        public bool IsDriver { get; set; }
+    }
+}
