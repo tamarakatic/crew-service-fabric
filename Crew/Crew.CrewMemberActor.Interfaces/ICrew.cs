aeee44feafd345f2479ebd802daff69bd24f522b
tamarakatic <tamarakatic@gmail.com>
2017-06-05 17:02:55 +0200

    Create Crew Interface

---
+++ Crew/Crew.CrewMemberActor.Interfaces/ICrew.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    public interface ICrew
+    {
+        String Name { get; set; }
+        String Status { get; set; }
+        CrewLocation Location { get; set; }
+        List<Member> Members { get; set; }
+        List<Planned> PlannedAssignments { get; set; }
+        List<Unplanned> UnplannedAssignments { get; set; }
+        List<Vehicle> Vehicles { get; set; }
+        List<Company> Companies { get; set; }
+    }
+}
