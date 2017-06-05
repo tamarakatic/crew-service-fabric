7253d055d76351e4ca9d17c088c52434876ef3ac
tamarakatic <tamarakatic@gmail.com>
2017-06-05 16:56:17 +0200

    Create CrewLocation Interface

---
+++ Crew/Crew.CrewMemberActor.Interfaces/ICrewLocation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    public interface ICrewLocation
+    {
+        DateTime Timestamp { get; set; }
+        float Latitude { get; set; }
+        float Longitude { get; set; }
+    }
+}
