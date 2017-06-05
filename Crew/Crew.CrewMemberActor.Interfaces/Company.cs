fe930fa7757dca471ca94a979d1c133ea928f937
tamarakatic <tamarakatic@gmail.com>
2017-06-05 16:55:25 +0200

    Create Company

---
+++ Crew/Crew.CrewMemberActor.Interfaces/Company.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    [DataContract]
+    public class Company : ICompany
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Address { get; set; }
+
+        [DataMember]
+        public int Telephone { get; set; }
+
+        [DataMember]
+        public CompanyType CompanyType { get; set; }
+    }
+}
