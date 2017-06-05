a102b2b3086ca99b242035a16ea2f280381edf3f
tamarakatic <tamarakatic@gmail.com>
2017-06-05 16:55:45 +0200

    Create Vehicle

---
+++ Crew/Crew.CrewMemberActor.Interfaces/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    [DataContract]
+    public class Vehicle : IVehicle
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Status { get; set; }
+
+        [DataMember]
+        public VehicleType VehicleType { get; set; }
+    }
+}
