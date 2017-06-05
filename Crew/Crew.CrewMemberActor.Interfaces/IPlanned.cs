020af13df1776ca1df7664237f98ced8fdf12363
tamarakatic <tamarakatic@gmail.com>
2017-06-05 17:00:43 +0200

    Create Planned Iterface

---
+++ Crew/Crew.CrewMemberActor.Interfaces/IPlanned.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    public interface IPlanned
+    {
+        DateTime From { get; set; }
+        DateTime To { get; set; }
+    }
+}
