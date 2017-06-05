c5fad4ee31a8689e72928c6d7130ba0796a86d14
tamarakatic <tamarakatic@gmail.com>
2017-06-05 16:53:56 +0200

    Create Vehicle Interface

---
+++ Crew/Crew.CrewMemberActor.Interfaces/IVehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    public enum VehicleType
+    {
+        Car,
+        Van,
+        CarWithCaravan
+    }
+
+    public interface IVehicle
+    {
+        String Name { get; set; }
+        String Status { get; set; }
+        VehicleType VehicleType { get; set; }
+    }
+}
