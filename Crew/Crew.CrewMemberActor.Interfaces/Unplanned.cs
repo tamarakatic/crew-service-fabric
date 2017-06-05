1fa77d1a97d35cb6c31d6e9a14d8b147e4a4dd5e
tamarakatic <tamarakatic@gmail.com>
2017-06-05 17:02:24 +0200

    Create Unplanned

---
+++ Crew/Crew.CrewMemberActor.Interfaces/Unplanned.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    [DataContract]
+    public class Unplanned : Assignment, IUnplanned
+    {
+        [DataMember]
+        public string Problem { get; set; }
+
+        [DataMember]
+        public string Comment { get; set; }
+    }
+}
