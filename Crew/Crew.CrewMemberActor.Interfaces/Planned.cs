e68dda5e9911c00006e72debed5a26c96665cea0
tamarakatic <tamarakatic@gmail.com>
2017-06-05 17:01:01 +0200

    Create Planned

---
+++ Crew/Crew.CrewMemberActor.Interfaces/Planned.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    [DataContract]
+    public class Planned : Assignment, IPlanned 
+    {
+        [DataMember]
+        public DateTime From { get; set; }
+
+        [DataMember]
+        public DateTime To { get; set; }
+    }
+}
