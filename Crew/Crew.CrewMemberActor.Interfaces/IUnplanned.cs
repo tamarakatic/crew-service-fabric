8f8e82e4ca61d24ce5882b0c9e485610ffcb8468
tamarakatic <tamarakatic@gmail.com>
2017-06-05 17:01:29 +0200

    Create Unplanned interface

---
+++ Crew/Crew.CrewMemberActor.Interfaces/IUnplanned.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    public interface IUnplanned
+    {
+        String Problem { get; set; }
+        String Comment { get; set; }
+    }
+}
