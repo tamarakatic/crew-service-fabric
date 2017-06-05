6e82a8fc577eaa8d40240fb298ed373f9d4ac9f2
tamarakatic <tamarakatic@gmail.com>
2017-06-05 16:58:12 +0200

    Create CrewLocation

---
+++ Crew/Crew.CrewMemberActor.Interfaces/CrewLocation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    [DataContract]
+    public class CrewLocation : ICrewLocation
+    {
+        public CrewLocation(float longitude, float latitude, DateTime timestamp)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Timestamp = timestamp;
+        }
+
+        [DataMember]
+        public DateTime Timestamp { get; set; }
+
+        [DataMember]
+        public float Latitude { get; set; }
+
+        [DataMember]
+        public float Longitude { get; set; }
+    }
+}
