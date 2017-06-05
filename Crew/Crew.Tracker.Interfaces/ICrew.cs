bb1aebc6c0e7b4a28622584e36ebc30c46a2ae19
tamarakatic <tamarakatic@gmail.com>
2017-06-05 17:06:49 +0200

    Add CreateCrew and CreateCrewByName method

---
+++ Crew/Crew.Tracker.Interfaces/ICrew.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
+
+namespace Crew.Tracker.Interfaces
+{
+    public interface ICrew : IService
+    {
+        Task CreateCrew(CrewMemberActor.Interfaces.Crew crew);
+        Task<CrewMemberActor.Interfaces.Crew> GetCrewByName(string crewName);
+    }
+}
