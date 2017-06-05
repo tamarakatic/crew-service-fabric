96f4a92d49587115a67b36109914a2463ca47e08
tamarakatic <tamarakatic@gmail.com>
2017-06-05 16:58:59 +0200

    Create Assignment Interface

---
+++ Crew/Crew.CrewMemberActor.Interfaces/IAssignment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    public interface IAssignment
+    {
+        String Name { get; set; }
+        String Status { get; set; }
+        Boolean IsPriority { get; set; }
+        List<String> AffectedCustomers { get; set; }
+        AssignmentType AssignmentType { get; set; }
+    }
+    public enum AssignmentType
+    {
+        Construction,
+        ElectricalInstallation,
+        MaintenanceOfElectricalEquipment
+    }
+}
