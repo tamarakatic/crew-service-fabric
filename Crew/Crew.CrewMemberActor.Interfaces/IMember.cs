1a232c820d374de25e351b16d76fead0db2bb4c3
tamarakatic <tamarakatic@gmail.com>
2017-06-05 16:51:09 +0200

    Create Memeber Interface

---
+++ Crew/Crew.CrewMemberActor.Interfaces/IMember.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Crew.CrewMemberActor.Interfaces
+{
+    public interface IMember
+    {
+        String Name { get; set; }
+        Int32 Telephone { get; set; }
+        Boolean IsLead { get; set; }
+        Boolean IsDriver { get; set; }
+    }
+}
